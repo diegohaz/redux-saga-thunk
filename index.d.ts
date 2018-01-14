32a8ac736986105d548327bbca48bef6157e5dc3
Tauka <k.tauka@gmail.com>
2018-01-14 08:46:58 +0600

    Added Typescript typings

---
+++ index.d.ts
@@ -0,0 +1,34 @@
+import { Action } from "redux";
+
+/* *
+ * Redux Saga Thunk Action
+ * 
+ * ensures existence of [meta]: object in Redux Action with [thunk]: boolean
+ */
+export interface ReduxSagaThunkAction extends Action {
+    meta: {
+        thunk: boolean,
+        [x: string]: any 
+    }
+}
+
+/* *
+ * Redux Saga Thunk Middleware
+ */
+
+export const middleware: any;
+
+/* *
+ * Redux Saga Thunk Selectors
+ */
+type ReduxSagaThunkSelector = (state: any, name: string | string[]) => boolean;
+
+export const isPending: ReduxSagaThunkSelector;
+export const hasFailed: ReduxSagaThunkSelector;
+export const isDone: ReduxSagaThunkSelector;
+export const isComplete: ReduxSagaThunkSelector;
+
+/* *
+ * Redux Saga Thunk Reducer
+ */
+export const thunkReducer: any;
