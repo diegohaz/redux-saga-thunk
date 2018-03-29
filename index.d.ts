1a0f862d67550204cb34ea9db208c211e16a56e4
Diego Haz <hazdiego@gmail.com>
2018-03-29 13:26:19 -0300

    Update index.d.ts and add it to package files

--- index.d.ts
+++ index.d.ts
@@ -1,33 +1,33 @@
 import { Action } from "redux";
 
-/* *
+/**
  * Redux Saga Thunk Action
- * 
+ *
  * ensures existence of [meta]: object in Redux Action with [thunk]: boolean
  */
 export interface ReduxSagaThunkAction extends Action {
-    meta: {
-        thunk: boolean,
-        [x: string]: any 
-    }
+  meta: {
+    thunk: boolean;
+    [x: string]: any;
+  };
 }
 
-/* *
+/**
  * Redux Saga Thunk Middleware
  */
 export const middleware: any;
 
-/* *
+/**
  * Redux Saga Thunk Selectors
  */
 type ReduxSagaThunkSelector = (state: any, name: string | string[]) => boolean;
 
-export const isPending: ReduxSagaThunkSelector;
-export const hasFailed: ReduxSagaThunkSelector;
-export const isDone: ReduxSagaThunkSelector;
-export const isComplete: ReduxSagaThunkSelector;
+export const pending: ReduxSagaThunkSelector;
+export const failed: ReduxSagaThunkSelector;
+export const fulfilled: ReduxSagaThunkSelector;
+export const done: ReduxSagaThunkSelector;
 
-/* *
+/**
  * Redux Saga Thunk Reducer
  */
 export const reducer: any;
